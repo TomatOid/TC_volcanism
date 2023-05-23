f48f78d8680cd273cd8cd8b0a724878e622e8df3
Connor <connormc757@gmail.com>
2023-05-23 16:32:01 -0400

    finish first tutorial

--- Tutorials/MatlabTutorial_TCs/MatlabTutorial_TCs.m
+++ Tutorials/MatlabTutorial_TCs/MatlabTutorial_TCs.m
@@ -3,8 +3,8 @@
 
 % Please place your name and the date the script was last updated at the
 % top
-% Lizzie Wallace
-% 01/19/2022
+% Connor McCarty
+% 05/23/2023
 
 % What you need
 % 2 data files:
@@ -32,108 +32,135 @@ load 'attracks.mat';
 
 
 % Plot the first track
-figure(1);clf
+
+
 % Now plot the buoy data
-plot(longm(1,:),latm(1,:),'o-')
 
 % You will see a line that goes to [0,0]. since each track 
 % is a different length, zeros are placed at the end of each matrix to 
 % denote the end of the track. before plotting, convert zeros to NaNs
-latm(latm==0)=NaN;
-longm(longm==0)=NaN;
-vsm(vsm==0)=NaN;
+
+% to avoid potential bugs where the latitude or longitude happen to be zero
+% by chance instead of indicating termination, I will overcomplicate it
+
+% first create a boolean mask
+
+storm_count = size(longm, 1);
+pts_max = size(longm, 2);
+mask = ones(storm_count, 1);
+
+i = pts_max;
+while (i > 0)
+    long_col = longm(:, i);
+    lat_col = latm(:, i);
+    mask = mask & (long_col == 0) & (lat_col == 0);
+    long_col(mask) = NaN;
+    lat_col(mask) = NaN;
+    
+    longm(:, i) = long_col;
+    latm(:, i) = lat_col;
+    i = i - 1;
+end
 
 
 % Plot the first track again
-figure(1);clf
-% Now plot the track data
-plot(longm(1,:),latm(1,:),'o-')
+
+figure(1);
+clf
+
+plot(latm(1, :), longm(1, :), 'o-');
+
 
 %% PLOT A STORM TRACK
 
 % use plotmap, xlim and ylim to orient yourself
 % overlay the location of the genesis latitude and longitude (in a different
 % color), so you know which way the track travelled (remember to hold on)
-figure(1);clf
-plotmap; hold on;
-plot(longm(1,:),latm(1,:),'-')
-plot(longm(1,1),latm(1,1), 'g*-');
-xlabel ('Latitude')
-ylabel('Longitude')
-xlim([250 310])
-ylim([10 40])
+
 %% PLOTTING MORE THAN ONE STORM TRACK
 %now lets plot all the storms that happened in 1851
-indx_1851 = find(yeari==1851);
-
-figure(2);clf
-plotmap; hold on;
-%to plot more than one track we need a for loop
-for ii=1:length(indx_1851)
-plot(longm(ii,:),latm(ii,:),'-or')
-end %for ii
-xlabel ('Latitude')
-ylabel('Longitude')
-xlim([250 310])
-ylim([10 40])
-
 %you can see that three of these tracks only have one point
-indx1940 = find(yeari>1940);
+
+
+plotStormsByYears(1851, 1851, longm, latm, yeari, 2);
 
 %now can you plot all the storms that happened after 1940?
-figure(3);
-clf
+
+plotStormsByYears(1940, 9999, longm, latm, yeari, 3);
 
 %now can you plot only the genesis points of storms that happened after 1940?
-figure(4);
-clf
 
+plotStormsByYears(1940, 9999, longm(:, 1), latm(:, 1), yeari, 4);
 
 %% PLOT STORM FREQUENCY
 
 % to plot storm frequency, we need to know how many storm occurred each
 % year
-yr_obs = [min(yeari):max(yeari)];
-nTC = zeros(length(yr_obs),1);
 % use a for loop to count how many tropical cyclones occur each year
-for ii=1:length(yr_obs)
-    nTC(ii) = length(find(yeari == yr_obs(ii)));
-end %for ii
-
-figure(5);clf
-plot(yr_obs,nTC)
 
 
 % we often categorize storms based on their intensity using the saffir
 % simpson scale. https://en.wikipedia.org/wiki/Saffir%E2%80%93Simpson_scale
 
 %find the max wind speed (m/s) of each storm in our dataset
-max_vsm = max(vsm,[],2);
+
+max_wind = max(vsm, [], 2);
+first_year = yeari(1);
+last_year  = yeari(length(yeari));
+storms_per_year = zeros(last_year - first_year + 1, 1);
+
+for i = 1 : storm_count
+    year = yeari(i) - first_year + 1;
+    storms_per_year(year) = storms_per_year(year) + (max_wind(i) >= 33);
+end
 
 % can we plot how many hurricanes occured each year (cat 1 and above)
-nhurr = zeros(length(yr_obs),1);
-% use a for loop to count how many tropical cyclones occur each year
-for ii=1:length(yr_obs)
-    indx_hurr = find(yeari == yr_obs(ii));
-    nhurr(ii) = length(find(max_vsm(indx_hurr)>=33))
-end %for ii
 
-figure(6);clf
-plot(yr_obs,nhurr)
+figure(5);
+clf
+plot(first_year : last_year, storms_per_year);
+
+figure(6);
+clf
+histogram(storms_per_year);
+
+% use a for loop to count how many tropical cyclones occur each year
+
+
+
+
+
+
+
 
 % can you plot how many intense hurricanes occured each year (cat 3 and above)
 
 
 
-figure(7);clf
+
 
 %% HISTOGRAM OF HURRICANE INTENSITY
 
 %we may also want to plot the distribution of storm intensities
 % we can use a histogram to do this
-f = figure;
-histogram(max_vsm)
+
+
 
 % ksdensity is another matlab function that allows you to plot
 % distributions
 
+function plotStormsByYears(lower_year, upper_year, longm, latm, yeari, fig_num)
+    hold on;
+
+    figure(fig_num);
+    clf
+
+    plotmap;
+
+    index_range = find(yeari >= lower_year, 1) : find(yeari <= upper_year, 1, 'last');
+
+    for i = index_range
+        plot(longm(i, :), latm(i, :), 'o-g');
+        plot(longm(i, 1), latm(i, 1), 'x');
+    end
+end
