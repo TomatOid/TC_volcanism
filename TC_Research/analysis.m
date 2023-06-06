df74c4fae39c1bec277228332cfe4c1b16fd57dc
Connor <connormc757@gmail.com>
2023-06-06 13:09:37 -0400

    before changing variables

--- TC_Research/analysis.m
+++ TC_Research/analysis.m
@@ -1,7 +1,14 @@
+% TODO
+% Isolate single eruptions completely
+
 load 'volcano_data.mat'
+
+before = 3;
+after = 8;
 
 threshold = 0.07;
 eruption_times = time(find(diff(aod550 >= threshold) == 1));
+eruption_stops = time(find(diff(aod550 >= threshold) == -1));
 
 dir_indexes = find_nearest(eruption_times, event_time);
 
@@ -9,8 +16,9 @@ unfiltered_times = eruption_times;
 
 % remove eruptions that are shortly followed by another eruption
 % this should remove noise from the SEA analysis
-diff_filter = [diff(transpose(eruption_times)) inf] > 8;
-eruption_times = eruption_times(diff_filter);
+diff_filter_after  = [diff(transpose(eruption_times)) inf] > after;
+diff_filter_before = [inf diff(transpose(eruption_stops))] > before;
+eruption_times = eruption_times(diff_filter_before & diff_filter_after);
 
 % now use the matched datasets to bin eruption times into three
 % lattitude reigions
@@ -29,7 +37,7 @@ for i = 1 : length(eruption_times)
 end
 
 % only include the tropics and the north to reduce noise
-filtered_events = sort([north south]);
+filtered_events = sort([north tropics]);
 
 figure(1);
 clf;
@@ -46,6 +54,7 @@ yline(threshold, '--');
 xline(filtered_events);
 makepretty_axes('Year', 'Optical Aerosol Depth');
 %print('figure1','-dpng', '-r300');
+%% The slow part
 
 load '../Storm Sets/LMR21_Atl_storms.mat';
 storm_years = 850 : 1900;
@@ -55,9 +64,6 @@ freqyear = freqyear_LMR21_all(1 : length(storm_years));
 vmax_LMR21_all = sqrt(mean(reshape(vmax_LMR21_all .^ 2, [100, length(vmax_LMR21_all) / 100])));
 
 fy_time_series = transpose([storm_years; vmax_LMR21_all(1 : length(storm_years))]);
-
-before = 3;
-after = 8;
 
 [fy_events, fy_comp] = coral_sea(fy_time_series, round(filtered_events) - 849, before, after);
 
@@ -106,7 +112,7 @@ hold on;
 
 axis([-before, after, -inf, inf]);
 makepretty_axes('Lag (Years)', 'Change in RMS max wind speed');
-title(['SEA of Storm Intensity, No Tropics']);
+title(['SEA of Storm Intensity, No Other Eruptions in Window']);
 subtitle(['AOD threshold = ' num2str(threshold) ', N = ' num2str(n_events)]);
 xline(0, '--');
-print(['sea_intensity_ns_double_thr_' num2str(threshold) '.png'], '-dpng', '-r400');
+print(['sea_intensity_nt_winfilter_thr_' num2str(threshold) '.png'], '-dpng', '-r400');
